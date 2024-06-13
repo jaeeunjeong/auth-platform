f4b6ce768612940144072b9b3fa93e3c3c2b26c7
jaeeunjeong <jejeong000@gmail.com>
2024-06-13 09:46:21 +0900

    Init.

---
+++ src/main/java/com/jejeong/authplatform/AuthenticationAndUserManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jejeong.authplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndUserManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndUserManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jejeong/authplatform/AuthenticationAndUserManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jejeong.authplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndUserManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
